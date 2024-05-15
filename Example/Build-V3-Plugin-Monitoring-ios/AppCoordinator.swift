1eea3c28697d3c7aed05968a1858d76b8834bfc2
dhekra-zaied-bryj <126702084+dhekra-zaied-bryj@users.noreply.github.com>
2024-05-15 14:02:42 -0400

    Add example

---
+++ Example/Build-V3-Plugin-Monitoring-ios/AppCoordinator.swift
@@ -0,0 +1,48 @@
+//
+//  AppCoordinator.swift
+//  FASDKBuild-Plugin-Optimizers-ios
+//
+//  Created by Jérôme Morissard on 05/17/2022.
+//  Copyright (c) 2022 Jérôme Morissard. All rights reserved.
+//
+
+import Build_V3_Plugin_Monitoring_ios
+import FASDKBuild_ios
+import Foundation
+import WebKit
+
+class AppCoordinator: FABaseAppCoordinator, FirebasePerformanceDelegate {
+    func updateTraceAdditional(attributes: [String : String]) -> [String : String] {
+        return attributes
+    }
+    
+    func performanceTraceNameForSDKEvent(for event: Build_V3_Plugin_Monitoring_ios.SDKEvent) -> String {
+        return "SplashRemoved"
+    }
+    
+    func performanceTraceNameForWebViewDidFinish(webViewAtIndex: Int) -> String {
+        return "WebViewDidFinish"
+    }
+    
+    func performanceTraceNameForWebViewDOMContentLoaded(webViewAtIndex: Int) -> String {
+        return "WebViewDOMContentLoaded("
+    }
+    
+    override func getConfigurationName() -> String? {
+        return "configuration"
+    }
+
+    override func getPlugins() -> [FABasePlugin] {
+        var plugins = [FABasePlugin]()
+
+        let firebaseManager = FirebasePerformanceTracesManager()
+        firebaseManager.delegate = self
+
+        let debugManager = DebugPrintTracesManager()
+
+        let monitoringPlugin = PerformancesMonitoringPlugin(tracesManagers: [firebaseManager, debugManager])
+
+        plugins.append(monitoringPlugin)
+        return plugins
+    }
+}
