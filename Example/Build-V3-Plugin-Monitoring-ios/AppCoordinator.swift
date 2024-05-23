ace628f7df2c059bf4e4bc36ca5665d519236b71
Pedro Fernandes <v-pedro.fernandes@Bryj.ai>
2024-05-23 11:20:45 +0100

    Update example project

---
+++ Example/Build-V3-Plugin-Monitoring-ios/AppCoordinator.swift
@@ -0,0 +1,46 @@
+//
+//  AppCoordinator.swift
+//  Build-V3-Plugin-Monitoring-ios
+//
+//  Created by Pedro Fernandes on 17/05/2024.
+//  Copyright (c) 2024 Bryj.ai. All rights reserved.
+//
+
+import Build_V3_Plugin_Monitoring_ios
+import FASDKBuild_ios
+import Foundation
+import WebKit
+
+class AppCoordinator: FABaseAppCoordinator {
+    override func getConfigurationName() -> String? {
+        return "configuration"
+    }
+
+    override func getPlugins() -> [FABasePlugin] {
+        var plugins = [FABasePlugin]()
+
+        let firebaseManager = FirebasePerformanceTracesManager()
+        firebaseManager.delegate = self
+
+        plugins.append(PerformancesMonitoringPlugin(tracesManagers: [firebaseManager, DebugPrintTracesManager()]))
+        return plugins
+    }
+}
+
+extension AppCoordinator: FirebasePerformanceDelegate {
+    func updateTraceAdditional(attributes: [String : String]) -> [String : String] {
+        return attributes
+    }
+
+    func performanceTraceNameForSDKEvent(for event: Build_V3_Plugin_Monitoring_ios.SDKEvent) -> String {
+        return "SplashRemoved"
+    }
+
+    func performanceTraceNameForWebViewDidFinish(webViewAtIndex: Int) -> String {
+        return "WebViewDidFinish"
+    }
+
+    func performanceTraceNameForWebViewDOMContentLoaded(webViewAtIndex: Int) -> String {
+        return "WebViewDOMContentLoaded("
+    }
+}
---
+++ Example/Build-V3-Plugin-Monitoring-ios/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Build-V3-Plugin-Monitoring-ios
+//
+//  Created by Pedro Fernandes on 17/05/2024.
+//  Copyright (c) 2024 Bryj.ai. All rights reserved.
+//
+
+import FASDKBuild_ios
+import FirebaseCore
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: FABaseAppDelegate {
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+
+        appCoordinator = AppCoordinator(window: self.window!)
+        appCoordinator.start()
+
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
