59e96f90e1313ad206677cbcdfcb2d9ef3b430d5
Fernando Cani <v-fe.cani@bryj.ai>
2024-10-21 14:13:51 +0100

    SDK-395: Update function registerScriptMessageHandler

--- Build-V3-Plugin-Monitoring-ios/Classes/Core/PerformancesMonitoringPlugin+FAPluginSectionViewControllerLifeCycleDelegate.swift
+++ Build-V3-Plugin-Monitoring-ios/Classes/Core/PerformancesMonitoringPlugin+FAPluginSectionViewControllerLifeCycleDelegate.swift
@@ -10,7 +10,8 @@ import Foundation
 import WebKit
 
 extension PerformancesMonitoringPlugin: FAPluginSectionViewControllerLifeCycleDelegate {
-    public func lifeCycleWebViewWillBeInitialized(sectionViewController: FASectionViewController, configuration: WKWebViewConfiguration) {}
+
+    public func lifeCycleWebViewWillBeInitialized(sectionViewController: FASectionViewController, configuration: WKWebViewConfiguration) { }
     
     public func lifeCycleWebViewInitialized(sectionViewController: FASectionViewController) {
         let handlers: [String] = [
@@ -19,13 +20,20 @@ extension PerformancesMonitoringPlugin: FAPluginSectionViewControllerLifeCycleDe
             PerformancesPluginScriptMessageHandler.performance_DocumentReadyStateIntractive,
             PerformancesPluginScriptMessageHandler.performance_load,
         ]
-        for handler in handlers {
-            sectionViewController.userContentController()?.removeScriptMessageHandler(forName: handler)
-            sectionViewController.userContentController()?.add(scriptMessageHandler, name: handler)
+        
+        handlers.forEach { handler in
+            self.registerScriptMessageHandler(sectionViewController.userContentController(), name: handler)
         }
+
         let classBundle = Bundle(for: PerformancesMonitoringPlugin.self)
-        if let bundle = Bundle(path: classBundle.bundlePath.appending("/Build-V3-Plugin-Monitoring-ios.bundle")) {
-            sectionViewController.webView()!.addJavascriptFileInjectionIfNeeded(resource: "interface_performances", forMainFrameOnly: true, bundle: bundle)
+        if let bundle = Bundle(path: classBundle.bundlePath.appending("/Build-V3-Plugin-Monitoring-ios.bundle")),
+            let webView = sectionViewController.webView() {
+            webView.addJavascriptFileInjectionIfNeeded(resource: "interface_performances", forMainFrameOnly: true, bundle: bundle)
         }
+    }
+
+    private func registerScriptMessageHandler(_ controller: WKUserContentController?, name: String) {
+        controller?.removeScriptMessageHandler(forName: name)
+        controller?.add(self.scriptMessageHandler, name: name)
     }
 }
